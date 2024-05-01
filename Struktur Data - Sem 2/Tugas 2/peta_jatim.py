8536aab0f9219b63585a7b0a5812720b4c57d3f1
DitryaTryandhana <149044989+DitryaTryandhana@users.noreply.github.com>
2024-05-01 17:12:39 +0700

    Update peta_jatim.py

--- Struktur Data - Sem 2/Tugas 2/peta_jatim.py
+++ Struktur Data - Sem 2/Tugas 2/peta_jatim.py
@@ -11,6 +11,18 @@ class Peta:
             self.cityList[city] = []
             return True
         return False
+        
+    def hapuscity(self,cityRemoved):
+     #cek apakah city yang ingin dihapus ada di list
+        if cityRemoved in self.cityList:
+        #iterasi setiap otherCity untuk hapus cityRemoved
+            for otherCity in self.cityList:
+                #cek apakah city yang ingin dihapus ada jalannya ke otherCity
+                if cityRemoved in self.cityList[otherCity]:
+                    self.cityList[otherCity].remove(cityRemoved)
+            del self.cityList[cityRemoved]
+            return True
+        return False
     
     def tambahkanJalan(self,city1,city2):
         if city1 in self.cityList and city2 in self.cityList:
@@ -35,5 +47,11 @@ petaJatim.tambahkanJalan("Probolinggo","Pasuruan")
 petaJatim.tambahkanJalan("Bondowoso","Probolinggo")
 petaJatim.tambahkanJalan("Situbondo","Bondowoso")
 petaJatim.tambahkanJalan("Banyuwangi","Situbondo")
+petaJatim.tambahkanJalan("Jember","Banyuwangi")
+petaJatim.tambahkanJalan("Lumajang","Jember")
+petaJatim.tambahkanJalan("Malang","Lumajang")
+petaJatim.tambahkanJalan("Tulungagung","Malang")
+petaJatim.tambahkanJalan("Kediri","Tulungagung")
+petaJatim.tambahkanJalan("Surabaya","Kediri")
 
 petaJatim.print()
